76ac1d59be69dac72fd6481416420a21c2dfa1fa
alex <alex@robot>
2017-10-07 19:08:41 -0700

    test app working

--- test.c
+++ test.c
@@ -24,18 +24,44 @@ int main() {
         fprintf(stderr,"opened fd %d for %s\n",
                 fd, path);
 
-        int angle = -1;
         //int angle[5] = { -1, -1, -1, -1, -1 };
+        struct servo_state state[6];
 
-        int ret = ioctl(fd, SERVO_IOC_GET_ANGLE, &angle);
+        int ret = ioctl(fd, SERVO_IOC_GET_STATE, &state);
 
         if (ret != 0) {
             fprintf(stderr, "ioctl returns %d: %s\n", errno, strerror(errno));
         } else {
-            fprintf(stderr, "success: angle = %d\n", angle);
+            fprintf(stderr, "base %d, shoulder %d, elbow %d, wrist1 %d, wrist2 %d, claw %d\n",
+                    state[0].enabled ? state[0].angle : -1,
+                    state[1].enabled ? state[1].angle : -1,
+                    state[2].enabled ? state[2].angle : -1,
+                    state[3].enabled ? state[3].angle : -1,
+                    state[4].enabled ? state[4].angle : -1,
+                    state[5].enabled ? state[5].angle : -1);
         }
 
+        state[0].enabled = 1;
+        state[1].enabled = 1;
+        state[2].enabled = 1;
+        state[3].enabled = 1;
+        state[4].enabled = 1;
+        state[5].enabled = 1;
 
+
+        ret = ioctl(fd, SERVO_IOC_SET_STATE, &state);
+
+        if (ret != 0) {
+            fprintf(stderr, "ioctl returns %d: %s\n", errno, strerror(errno));
+        } else {
+            fprintf(stderr, "base %d, shoulder %d, elbow %d, wrist1 %d, wrist2 %d, claw %d\n",
+                    state[0].enabled ? state[0].angle : -1,
+                    state[1].enabled ? state[1].angle : -1,
+                    state[2].enabled ? state[2].angle : -1,
+                    state[3].enabled ? state[3].angle : -1,
+                    state[4].enabled ? state[4].angle : -1,
+                    state[5].enabled ? state[5].angle : -1);
+        }
         close(fd);
     }
     return 0;
