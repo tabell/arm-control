873eb6f9d9769bbb0a9924cac4f46e78d1bb4836
Alex Bell <t.alexander.bell@gmail.com>
2017-10-07 17:16:35 -0700

    change ioctls to states

--- pwm-servo.h
+++ pwm-servo.h
@@ -5,14 +5,17 @@
 
 #define SERVO_IOC_MAGIC 'Q'
 #define SERVO_IOC_RESET     _IO(SERVO_IOC_MAGIC, 0)
-#define SERVO_IOC_SET_ANGLE _IOW(SERVO_IOC_MAGIC, 1, int*)
-#define SERVO_IOC_GET_ANGLE _IOR(SERVO_IOC_MAGIC, 2, int*)
-#define SERVO_IOC_ENABLE    _IOW(SERVO_IOC_MAGIC, 3, int*)
-#define SERVO_IOC_DISABLE   _IOW(SERVO_IOC_MAGIC, 4, int*)
+#define SERVO_IOC_SET_STATE _IOW(SERVO_IOC_MAGIC, 1, void*)
+#define SERVO_IOC_GET_STATE _IOR(SERVO_IOC_MAGIC, 2, void*)
 #define SERVO_IOC_MAX 8
 
 #define SERVO_MAJ 0
 
 #define PWM_SERVO_CLASS_NAME "servos"
+
+typedef struct servo_state {
+    int angle;
+    bool enabled;
+} tState;
 
 #endif /* PWM_SERVO_H */
