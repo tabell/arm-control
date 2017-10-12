69c86984b1b924342fd570495b2d015dcd843b3f
alex <alex@robot>
2017-10-11 20:24:10 -0700

    sweep command remembers old position

--- user/sweep.c
+++ user/sweep.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/ioctl.h>
@@ -10,9 +11,10 @@
 
 #include "servo.h"
 
-#define DEF_DUTY 711000
+#define DEF_DUTY 900000
 
 int fd = -1;
+const char *path = "/dev/robot";
 
 typedef enum command {
     COMMAND_UNKNOWN,
@@ -26,25 +28,36 @@ typedef enum command {
 
 typedef struct node {
     int index;
-    int max_duty;
     int min_duty;
-    int duty_offset;
+    int max_duty;
     int duty;
+    int a;
+    int b;
 } tNode;
 
-tNode node2 = { 2, 700000, 1400000, 1000000, DEF_DUTY };
+tNode node[] = {{ 0, 600000, 2400000, DEF_DUTY, 10000, 0 },
+                { 1, 600000, 2600000, DEF_DUTY, 10000, 0 },
+                { 2, 200000, 2400000, DEF_DUTY, 10000, 0 },
+                { 3, 200000, 2400000, DEF_DUTY, 10000, 0 },
+                { 4, 200000, 2400000, DEF_DUTY, 10000, 0 },
+                { 5, 200000, 2400000, DEF_DUTY, 10000, 0 }};
 
 int setDuty(tNode* node, int duty)
 {
+    if (duty > node->max_duty) duty = node->max_duty;
+    if (duty < node->min_duty) duty = node->min_duty;
     struct servo_ioctl_pkt pkt;
+    //printf("before duty = %d ", node->duty);
     memset(&pkt, 0, sizeof(pkt));
     pkt.idx = node->index;
     pkt.duty_ns = duty;
 
     if (0 != (ioctl(fd, SERVO_IOC_SET_DUTY_NS, &pkt))) {
+        printf("Error %d calling set ioctl: %s\n", errno, strerror(errno));
         return -errno;
     }
     node->duty = duty;
+    //printf("after duty = %d\n", node->duty);
     return 0;
 }
 
@@ -76,49 +89,44 @@ int servoSync(tNode* node)
 
 }
 
-int sweep(tNode *node, int duty_start, int duty_end, float duration)
+int sweep(tNode *node, int duty_end)
 {
     int ret = 0;
 
+    int duty_start = node->duty;
+
+    if (duty_start == duty_end) {
+        return 0;
+    }
+
     fprintf(stderr, "sweeping node %d from %d to %d in %f s\n",
-            node->index, duty_start, duty_end, duration);
+            node->index, duty_start, duty_end);
+
     
     int duty_delta = duty_end - duty_start;
     int num_steps = abs(duty_delta / 4000);
-    fprintf(stderr, "num_steps = %d\n", num_steps);
 
     int duty_step = duty_delta / num_steps;
-    fprintf(stderr, "duty: delta = %d step = %d\n", duty_delta, duty_step);
+    float duration = abs(duty_delta / 400000);
     float time_delta = (duration * 1E9);
     int time_step = time_delta  / num_steps;
-    fprintf(stderr, "time: delta = %f ns step = %d ns\n", time_delta, time_step);
+    fprintf(stderr, "steps = %d, duty step = %d, time ns step = %d ms, duration = %.2f s\n",
+            num_steps, duty_step, time_step / 1000000, ((float)time_step * num_steps) / 1E9);
 
     struct timespec delay = {
         .tv_sec = 0,
         .tv_nsec = abs(time_step),
     };
 
-    const char *path = "/dev/robot";
 
-    fprintf(stderr, "trying to open %s\n", path);
-    fd = open(path,
-            O_WRONLY|O_CREAT|O_TRUNC|O_CLOEXEC,
-            S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
-    if (fd < 0) {
-        fprintf(stderr, "Error %d opening %s: %s\n",
-                fd, path, strerror(fd));
-    } else {
-
-        fprintf(stderr,"opened fd %d for %s\n",
-                fd, path);
         int duty = duty_start;
         fprintf(stderr, "start = %d goal = %d step = %d\n",
                 duty, duty_end, duty_step);
 
         int step = 0;
-        for (duty = duty_start; abs(duty - duty_end) > abs(duty_step); duty += duty_step) {
-      //  fprintf(stderr, "abs(duty - duty_end) = %d, abs(duty_step) = %d\n",
-      //          abs(duty - duty_end), abs(duty_step));
+        //for (duty = duty_start; abs(duty - duty_end) > abs(duty_step); duty += duty_step) {
+        for (step = 0; step < num_steps; step++) {
+            duty += duty_step;
 #ifdef DEBUG_PRINT
             if (step++ % 20 == 0) {
                 fprintf(stderr, "%d: start = %d current = %d end = %d step = %d\n",
@@ -140,35 +148,53 @@ int sweep(tNode *node, int duty_start, int duty_end, float duration)
         }
 
 
-
-        close(fd);
-    }
-
-    fprintf(stderr, "clean exit\n");
     return ret;
 }
 
 int main(int argc, char** argv) {
-    if (argc < 3) {
-        fprintf(stderr,
-                "usage: %s <index 1-6> <duty> <time>\n", argv[0]);
-        return 0;
-    }
-
     /* Allocation */
     int ret;
+    int index = -1;
+    int duty_end = -1;
+    bool setting = false;
 
-    /* parse index */
-    int index = strtoul(argv[1], NULL, 10);
-    if ((index > 5) || (index < 0)) {
-        fprintf(stderr,"index out of range\n");
+    /* Parse arguments */
+    if (argc > 1) {
+        /* parse index */
+        index = strtoul(argv[1], NULL, 10);
+        if ((index > 5) || (index < 0)) {
+            fprintf(stderr,"index out of range\n");
+            return 0;
+        }
+    } else {
+        fprintf(stderr,
+                "usage: %s <index 1-6> [<duty>]\n", argv[0]);
         return 0;
     }
 
-    int duty_end = strtoul(argv[2], NULL, 10);
+    if (argc > 2) {
+        duty_end = strtoul(argv[2], NULL, 10);
+        setting = true;
+    }
 
-    float duration = strtof(argv[3], NULL);
 
-    sweep(&node2, 711000, duty_end, duration);
-    sweep(&node2, duty_end, 711000, duration);
+    fd = open(path,
+            O_WRONLY|O_CREAT|O_TRUNC|O_CLOEXEC,
+            S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
+    if (fd < 0) {
+        fprintf(stderr, "Error %d opening %s: %s\n",
+                fd, path, strerror(fd));
+    } else {
+        if (0 != (ret = getDuty(&node[index]))) {
+            printf("getDuty returns %d: %s",
+                    -ret, strerror(-ret));
+        } else {
+            if (setting) {
+                sweep(&node[index], duty_end * node[index].a + node[index].b);
+            }
+            printf("duty: %d\n", (node[index].duty - node[index].b) / node[index].a);
+        }
+        close(fd);
+    }
+    return 0;
 }
