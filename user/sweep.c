246757595021dc63fa428f8c8efc130b6c84669d
alex <alex@robot>
2017-10-11 18:29:41 -0700

    sweep working with node struct

--- user/sweep.c
+++ user/sweep.c
@@ -10,6 +10,8 @@
 
 #include "servo.h"
 
+#define DEF_DUTY 711000
+
 int fd = -1;
 
 typedef enum command {
@@ -21,38 +23,50 @@ typedef enum command {
     CMD_SYNC,
 } tCmd;
 
-int setDuty(int index, int duty)
+
+typedef struct node {
+    int index;
+    int max_duty;
+    int min_duty;
+    int duty_offset;
+    int duty;
+} tNode;
+
+tNode node2 = { 2, 700000, 1400000, 1000000, DEF_DUTY };
+
+int setDuty(tNode* node, int duty)
 {
     struct servo_ioctl_pkt pkt;
     memset(&pkt, 0, sizeof(pkt));
-    pkt.idx = index;
+    pkt.idx = node->index;
     pkt.duty_ns = duty;
 
     if (0 != (ioctl(fd, SERVO_IOC_SET_DUTY_NS, &pkt))) {
         return -errno;
     }
+    node->duty = duty;
     return 0;
 }
 
-int getDuty(int index, int* duty)
+int getDuty(tNode* node)
 {
     struct servo_ioctl_pkt pkt;
     memset(&pkt, 0, sizeof(pkt));
-    pkt.idx = index;
+    pkt.idx = node->index;
 
     if (0 != (ioctl(fd, SERVO_IOC_GET_DUTY_NS, &pkt))) {
         return -errno;
     }
-    *duty = pkt.duty_ns;
+    node->duty = pkt.duty_ns;
     return 0;
 
 }
 
-int servoSync(int index)
+int servoSync(tNode* node)
 {
     struct servo_ioctl_pkt pkt;
     memset(&pkt, 0, sizeof(pkt));
-    pkt.idx = index;
+    pkt.idx = node->index;
 
     if (0 != (ioctl(fd, SERVO_IOC_SYNC, &pkt))) {
         return -errno;
@@ -61,35 +75,18 @@ int servoSync(int index)
 
 
 }
-int main(int argc, char** argv) {
-    if (argc < 4) {
-        fprintf(stderr,
-                "usage: %s <index 1-6> <min duty> <max duty> <time>\n", argv[0]);
-        return 0;
-    }
 
-    /* Allocation */
-    int ret;
-
-    /* parse index */
-    int index = strtoul(argv[1], NULL, 10);
-    if ((index > 5) || (index < 0)) {
-        fprintf(stderr,"index out of range\n");
-        return 0;
-    }
-
-    int duty_start = strtoul(argv[2], NULL, 10);
-    int duty_end = strtoul(argv[3], NULL, 10);
-
-    float duration = strtof(argv[4], NULL);
+int sweep(tNode *node, int duty_start, int duty_end, float duration)
+{
+    int ret = 0;
 
     fprintf(stderr, "sweeping node %d from %d to %d in %f s\n",
-            index, duty_start, duty_end, duration);
+            node->index, duty_start, duty_end, duration);
     
-    int num_steps = duration * 100;
+    int duty_delta = duty_end - duty_start;
+    int num_steps = abs(duty_delta / 4000);
     fprintf(stderr, "num_steps = %d\n", num_steps);
 
-    int duty_delta = duty_end - duty_start;
     int duty_step = duty_delta / num_steps;
     fprintf(stderr, "duty: delta = %d step = %d\n", duty_delta, duty_step);
     float time_delta = (duration * 1E9);
@@ -118,14 +115,20 @@ int main(int argc, char** argv) {
         fprintf(stderr, "start = %d goal = %d step = %d\n",
                 duty, duty_end, duty_step);
 
-        fprintf(stderr, "abs(duty - duty_end) = %d, abs(duty_step) = %d\n", abs(duty - duty_end), abs(duty_step));
+        int step = 0;
         for (duty = duty_start; abs(duty - duty_end) > abs(duty_step); duty += duty_step) {
-            fprintf(stderr, "start = %d current = %d end = %d step = %d\n",
-                    duty_start, duty, duty_end, duty_step);
-            if (0 != (ret = setDuty(index, duty))) {
+      //  fprintf(stderr, "abs(duty - duty_end) = %d, abs(duty_step) = %d\n",
+      //          abs(duty - duty_end), abs(duty_step));
+#ifdef DEBUG_PRINT
+            if (step++ % 20 == 0) {
+                fprintf(stderr, "%d: start = %d current = %d end = %d step = %d\n",
+                        step, duty_start, duty, duty_end, duty_step);
+            }
+#endif
+            if (0 != (ret = setDuty(node, duty))) {
                 fprintf(stderr, "Error %d setting duty %d for id %d: %s\n",
                         ret, duty, index, strerror(-ret));
-            } else if (0 != (ret = servoSync(index))) {
+            } else if (0 != (ret = servoSync(node))) {
                 fprintf(stderr, "Error %d syncing for id %d: %s\n",
                         ret, index, strerror(-ret));
             } else if (0 != (ret = nanosleep(&delay, NULL))) {
@@ -133,7 +136,7 @@ int main(int argc, char** argv) {
                         ret, strerror(-ret));
             }
 
-            if (!ret) break;
+            if (ret) break;
         }
 
 
@@ -142,5 +145,30 @@ int main(int argc, char** argv) {
     }
 
     fprintf(stderr, "clean exit\n");
-    return 0;
+    return ret;
+}
+
+int main(int argc, char** argv) {
+    if (argc < 3) {
+        fprintf(stderr,
+                "usage: %s <index 1-6> <duty> <time>\n", argv[0]);
+        return 0;
+    }
+
+    /* Allocation */
+    int ret;
+
+    /* parse index */
+    int index = strtoul(argv[1], NULL, 10);
+    if ((index > 5) || (index < 0)) {
+        fprintf(stderr,"index out of range\n");
+        return 0;
+    }
+
+    int duty_end = strtoul(argv[2], NULL, 10);
+
+    float duration = strtof(argv[3], NULL);
+
+    sweep(&node2, 711000, duty_end, duration);
+    sweep(&node2, duty_end, 711000, duration);
 }
