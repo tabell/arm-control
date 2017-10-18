22564c8ee06fbe17fd79d9ffff221e42369524f2
alex <alex@robot>
2017-10-17 21:46:20 -0700

    cleaned up path code, added timing and better logging

--- user/sweep.c
+++ user/sweep.c
@@ -9,10 +9,14 @@
 #include <errno.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h> /* struct timespec and nanosleep */
+#include <assert.h>
 
 #include "servo.h"
 
 #define DEF_DUTY 900000
+
+#define pr(fmt, ...) fprintf(stderr, "<%s:%d> " fmt "\n", __func__, __LINE__, ##__VA_ARGS__)
 
 //#define DRY_RUN
 //#define DEBUG_PRINT
@@ -36,6 +40,8 @@ typedef struct path {
     int start_duty;
     int target_duty;
     float progress;
+    float progress_step;
+    int num_steps;
     tPathFunc path_func; 
 } tPath;
 
@@ -44,17 +50,18 @@ typedef struct node {
     int min_duty;
     int max_duty;
     int duty;
+    int last_duty;
     int a;
     int b;
     tPath *path;
 } tNode;
 
-tNode node[] = {{ 0, 600000, 2400000, DEF_DUTY, 10000, 0 },
-                { 1, 600000, 2600000, DEF_DUTY, 10000, 0 },
-                { 2, 600000, 2400000, DEF_DUTY, 10000, 0 },
-                { 3, 600000, 2400000, DEF_DUTY, 10000, 0 },
-                { 4, 600000, 2400000, DEF_DUTY, 10000, 0 },
-                { 5, 600000, 2400000, DEF_DUTY, 10000, 0 }};
+tNode g_node[] = {{   .index = 0, .min_duty = 600000, .max_duty = 2400000, .duty = DEF_DUTY, .a = 10000, .b = 0 },
+                    { .index = 1, .min_duty = 600000, .max_duty = 2600000, .duty = DEF_DUTY, .a = 10000, .b = 0 },
+                    { .index = 2, .min_duty = 600000, .max_duty = 2400000, .duty = DEF_DUTY, .a = 10000, .b = 0 },
+                    { .index = 3, .min_duty = 600000, .max_duty = 2400000, .duty = DEF_DUTY, .a = 10000, .b = 0 },
+                    { .index = 4, .min_duty = 600000, .max_duty = 2400000, .duty = DEF_DUTY, .a = 10000, .b = 0 },
+                    { .index = 5, .min_duty = 1800000, .max_duty = 2400000, .duty = DEF_DUTY, .a = 10000, .b = 0 }};
 
 float gentle(float in)
 {
@@ -88,10 +95,12 @@ int calcNextDuty(tNode* node)
     } else {
         step = delta * node->path->path_func(node->path->progress);
     }
+    node->last_duty = node->duty;
     node->duty = base + step;
+    //pr( "diff = %d", node->duty - node->last_duty);
     return 0;
 }
-
+#if 0
 int setAllDuty(tNode *nodes[], float progress)
 {
 
@@ -99,35 +108,36 @@ int setAllDuty(tNode *nodes[], float progress)
     int i;
     for (i = 0; i < 6; i++) {
         if (!nodes[i]) {
-            printf("error node %d null\n", i);
+            pr("error node %d null", i);
             return -EINVAL;
         }
         nodes[i]->path->progress = progress;
         if (0 != (ret = calcNextDuty(nodes[i]))) {
-            printf("Error %d setting node %d: %s\n",
+            pr("Error %d setting node %d: %s",
                     -ret, i, strerror(-ret));
             break;
         } else if (0 != (ret = setDuty(nodes[i]))) {
-            printf("Error %d setting node %d: %s\n",
+            pr("Error %d setting node %d: %s",
                     -ret, i, strerror(-ret));
             break;
         }
     }
     return ret;
 }
+#endif
 
 int setDuty(tNode* node)
 {
     if (node->duty > node->max_duty) node->duty = node->max_duty;
     if (node->duty < node->min_duty) node->duty = node->min_duty;
     struct servo_ioctl_pkt pkt;
-    //printf("before duty = %d ", node->duty);
+    //pr("before duty = %d ", node->duty);
     memset(&pkt, 0, sizeof(pkt));
     pkt.idx = node->index;
     pkt.duty_ns = node->duty;
 #ifndef DRY_RUN
     if (0 != (ioctl(fd, SERVO_IOC_SET_DUTY_NS, &pkt))) {
-        printf("Error %d calling set ioctl: %s\n", errno, strerror(errno));
+        pr("Error %d calling set ioctl: %s", errno, strerror(errno));
         return -errno;
     }
 #endif
@@ -165,68 +175,111 @@ int servoSync(tNode* node)
 
 }
 
-int sweep(tNode *node, int duty_end)
+int setPath(
+        tPath** pPath,
+        int start_duty,
+        int duty_goal,
+        struct timespec *frameDelay)
 {
-    int ret = 0;
+    if (!path || !frameDelay) return -EINVAL;
+    tPath *path = NULL;
+    if (NULL == (path = malloc(sizeof(tPath)))) {
+        pr( "couldn't allocate memory for path struct");
+        return -ENOMEM;
+    }
 
-    int duty_start = node->duty;
+    path->start_duty = start_duty;
+    path->target_duty = duty_goal;
+    path->progress_step = fabs((float)DUTY_STEP / (float)(duty_goal - start_duty));
 
+//    frameDelay->tv_nsec = 2222222;
+#if 0
     if (abs(duty_start - duty_end) < 100) {
         return 0;
     }
-#ifdef DEBUG_PRINT
-    fprintf(stderr, "sweeping node %d from %d to %d\n",
-            node->index, duty_start, duty_end);
-#endif
     
-    int duty_delta = duty_end - duty_start;
-    printf("duty_delta = duty_end (%d) - duty_start(%d) = %d\n", duty_end, duty_start, duty_delta);
+    int duty_delta = duty_goal - duty_start;
+    pr("duty_delta = duty_end (%d) - duty_start(%d) = %d", 
+            duty_goal, duty_start, duty_delta);
     int num_steps = abs(duty_delta / 3999);
-    printf(" num_steps = abs(duty_delta / 3999) = %d\n", num_steps);
-    float progress_step = (float)DUTY_STEP / (float)duty_delta;
-    printf("progress_step = duty_step / duty_delta = %f\n", progress_step);
-    float duration = fabs((float)duty_delta / (float)1800000);
-    printf("duration = abs(duty_delta / 1800000) = %f\n", duration);
-    float time_delta = (duration * 1E9);
-    printf("time_delta = (duration * 1E9) = %d\n", time_delta);
-#if 0
-    fprintf(stderr, "steps = %d, duty step = %d, time ns step = %d ms, duration = %.2f s\n",
-            num_steps, duty_step, time_step / 1000000, ((float)time_step * num_steps) / 1E9);
-#endif
+    pr("num_steps = abs(duty_delta / 3999) = %d", num_steps);
+    pr("progress_step = duty_step / duty_delta = %f", progress_step);
+    float duration = fabs((float)duty_delta / (float)1800);
+    pr("duration = abs(duty_delta / 1800000) = %f", duration);
+    float time_delta = (duration * 1E3);
+    pr("time_delta = (duration * 1E9) = %d", time_delta);
     int time_step = time_delta  / num_steps;
-    printf("time_step = time_delta  / num_steps = %d\n", time_step);
-    struct timespec delay = {
-        .tv_sec = 0,
-        .tv_nsec = abs(time_step),
-    };
+    pr("time_step = time_delta  / num_steps = %d", time_step);
+#endif
+    //tPath path = { duty_start, duty_goal, 0, NULL };
 
 
-    int duty = duty_start;
+    path->path_func = gentle2;
 
-    tPath path = { duty_start, duty_end, 0, NULL };
-    path.path_func = gentle2;
+    *pPath = path;
 
-    node->path = &path;
+    return 0;
+}
 
-    int step = 0;
-    //for (duty = duty_start; abs(duty - duty_end) > abs(duty_step); duty += duty_step) {
-    for (step = 0; step < num_steps; step++) {
-        node->path->progress += progress_step;
-        calcNextDuty(node);
-        if (0 != (ret = setDuty(node))) {
-            fprintf(stderr, "Error %d setting duty %d for id %d: %s\n",
-                    ret, duty, index, strerror(-ret));
-        } else if (0 != (ret = servoSync(node))) {
-            fprintf(stderr, "Error %d syncing for id %d: %s\n",
-                    ret, index, strerror(-ret));
-        } else if (0 != (ret = nanosleep(&delay, NULL))) {
-            fprintf(stderr, "error %d calling nanosleep: %s\n",
-                    ret, strerror(-ret));
-        }
+float clock_delta(
+        struct timespec t1,
+        struct timespec t2)
+{
+    int d_s = t2.tv_sec - t1.tv_sec;
+    int d_ns = t2.tv_nsec - t1.tv_nsec;
+    return d_s ? d_s * 1E9 + d_ns : d_ns;
+}
+int sweep(tNode *node, int duty_end)
+{
+    int ret = 0;
+    pr("duty_end = %d", duty_end);
 
-        if (ret) break;
+    struct timespec delay = { 0 };
+
+    if (0 != (ret = setPath(&node->path, node->duty, duty_end, &delay))) {
+        pr( "error %d calculating params: %s",
+                ret, strerror(-ret));
+        return ret;
     }
 
+    int step_count = 0;
+    struct timespec start_time, end_time;
+    clock_gettime(CLOCK_REALTIME, &start_time);
+    do {
+        step_count++;
+        node->path->progress += node->path->progress_step;
+        calcNextDuty(node);
+        if (0 != (ret = setDuty(node))) {
+            pr( "Error %d setting duty %d for id %d: %s",
+                    ret, node->duty, node->index, strerror(-ret));
+        } else if (0 != (ret = servoSync(node))) {
+            pr( "Error %d syncing for id %d: %s",
+                    ret, node->index, strerror(-ret));
+        } else if (0 != (ret = nanosleep(&delay, NULL))) {
+            pr( "error %d calling nanosleep s=%d ns=%d: %s",
+                    ret, delay.tv_sec, delay.tv_nsec, strerror(-ret));
+        }
+
+        //pr( "duty = %d goal = %d progress = %.2f",
+        //        node->duty, node->path->target_duty, node->path->progress);
+        assert(node->path->target_duty > 0);
+
+        if (ret) break;
+        if (node->path->progress < 0 || node->path->progress > 1)
+        {
+            break;
+        }
+    } while (abs(node->path->target_duty - node->duty) > 100);
+
+    clock_gettime(CLOCK_REALTIME, &end_time);
+    float duration = clock_delta(start_time, end_time);
+    float step_duration = duration / step_count;
+    pr("took %d steps in %.2f ms (%.2f ms/step)", 
+            step_count, duration / 1E6, step_duration / 1E6);
+    if (node->path) {
+        free(node->path);
+        node->path = NULL;
+    }
 
     return ret;
 }
@@ -243,12 +296,11 @@ int main(int argc, char** argv) {
         /* parse index */
         index = strtoul(argv[1], NULL, 10);
         if ((index > 5) || (index < 0)) {
-            fprintf(stderr,"index out of range\n");
+            pr("index out of range");
             return 0;
         }
     } else {
-        fprintf(stderr,
-                "usage: %s <index 1-6> [<duty>]\n", argv[0]);
+        pr("usage: %s <index 1-6> [<duty>]", argv[0]);
         return 0;
     }
 
@@ -259,20 +311,20 @@ int main(int argc, char** argv) {
 
 
     fd = open(path,
-            O_WRONLY|O_CREAT|O_TRUNC|O_CLOEXEC,
+            O_WRONLY|O_CREAT|O_TRUNC,
             S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
     if (fd < 0) {
-        printf("Error %d opening %s: %s\n",
+        pr("Error %d opening %s: %s",
                 fd, path, strerror(fd));
     } else {
-        if (0 != (ret = getDuty(&node[index]))) {
-            printf("getDuty returns %d: %s",
+        if (0 != (ret = getDuty(&g_node[index]))) {
+            pr("getDuty returns %d: %s",
                     -ret, strerror(-ret));
         } else {
             if (setting) {
-                sweep(&node[index], duty_end * node[index].a + node[index].b);
+                sweep(&g_node[index], duty_end * g_node[index].a + g_node[index].b);
             }
-            printf("duty: %d\n", (node[index].duty - node[index].b) / node[index].a);
+            pr("duty: %d", (g_node[index].duty - g_node[index].b) / g_node[index].a);
         }
         close(fd);
     }
